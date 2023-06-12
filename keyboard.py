0ea2cfe1b3ff28f3538052eea7b67aa84694f1a1
Mustafa Imam <mustafa.imam@ontariotechu.net>
2023-06-12 18:31:26 -0400

    added delta t

--- keyboard.py
+++ keyboard.py
@@ -2,17 +2,19 @@ from tkinter import *
 import time
 
 def key_up(key_event):
+    global start_time
     print("Up", key_event.char)
     time_stamp = time.time()
-    print(time_stamp)
+    time_difference = time_stamp - start_time
+    print("Time Difference:", time_difference)
 
 def key_down(key_event):
-    print("down", key_event.char)
-    time_stamp = time.time()
-    print(time_stamp)
+    global start_time
+    print("Down", key_event.char)
+    start_time = time.time()
 
 root = Tk()
-frame = Frame(root, width = 100, height = 100)
+frame = Frame(root, width=100, height=100)
 frame.bind("<KeyPress>", key_down)
 frame.bind("<KeyRelease>", key_up)
 frame.pack()
